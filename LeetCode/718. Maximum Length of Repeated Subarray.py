596ca90fc885db673a1c05da498bf8f78fad4285
yangjin <13246856469@163.com>
2018-04-13 21:17:47 +0800

    add

---
+++ LeetCode/718. Maximum Length of Repeated Subarray.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+'''
+Given two integer arrays A and B, return the maximum length of an subarray that appears in both arrays.
+
+Example 1:
+Input:
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+Output: 3
+Explanation: 
+The repeated subarray with maximum length is [3, 2, 1].
+Note:
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+'''
+class Solution:
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        
